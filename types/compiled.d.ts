cc9b83da3316cba6d95408b8861f1a7d8a0253fe
fz6m <59400654+fz6m@users.noreply.github.com>
2023-10-30 05:04:25 +0800

    dep: upgrade and precompiled 🍒

---
+++ types/compiled.d.ts
@@ -0,0 +1,9 @@
+declare module '@xn-sakina/vary/compiled/sort-package-json' {
+  import * as p from 'sort-package-json';
+  export = p;
+}
+
+declare module '@xn-sakina/vary/compiled/chalk' {
+  import * as p from 'chalk';
+  export = p;
+}
